a9844c55ff89d06e3ac7e4bf9261a1c7593f09d8
svdoit <doemily@naver.com>
2020-09-08 16:40:01 +0900

    models created

--- dreamaryproject/urls.py
+++ dreamaryproject/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from page import views
 from django.conf import settings
-from django.conf.static import static
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
