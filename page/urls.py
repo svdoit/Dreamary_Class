b0fd1ababf8000b46b57ff0349093b942b6069a0
svdoit <doemily@naver.com>
2020-09-09 04:11:47 +0900

    delete & create Service Created

--- page/urls.py
+++ page/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name = "home"),
     path('introduce/', views.introduce, name = "introduce"),
-    path('profile/<int:designer_id>', views.detail, name = "detail"),
+    path('profile/<int:designer_id>/', views.detail, name = "detail"),
+    path('new/', views.new, name = "new"),
+    path('create/', views.create, name = "create"),
+    path('delete/<int:designer_id>/', views.delete, name = "delete"),
 ]
--- page/views.py
+++ page/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Designer
 
 # Create your views here.
@@ -12,3 +12,28 @@ def introduce(request):
 def detail(request, designer_id):
     designer = get_object_or_404(Designer, pk = designer_id)
     return render(request, 'detail.html', {'designer' : designer})
+
+def new(request):
+    return render(request, 'new.html')
+
+def create(request):
+    if request.method == "POST":
+        post = Designer()
+
+        if 'image' in request.FILES:
+            post.image = request.FILES['image']
+        post.name = request.POST['name']
+        post.address = request.POST['address']
+        post.description = request.POST['description']
+
+        post.save()
+
+        return redirect('detail', post.id)
+        # 위의 코드가 return redirect('profile/' + str(pst.id)) 임!! 위의 코드처럼 쓰는 걸 더 추천한다고..
+        # detail은 urls.py의 detail임
+
+def delete(request, designer_id):
+    post = get_object_or_404(Designer, pk = designer_id)
+    post.delete()
+
+    return redirect('home')
